fbb45a686929f9cb7283518e552c2adc33c14426
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-11-29 22:01:22 +0530

    Basic Menu

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import { Component } from "react";
 
 class App extends Component {
 	render() {
-		return <div className="Menu"></div>;
+		return (
+			<div className="Menu">
+				<div id="Utils">Item 1</div>
+				<div id="MusicPlayer">Item 2</div>
+				<div id="LockOrianation">Item 3</div>
+				<div id="NightLight">Item 4</div>
+				<div id="Brightness">Item 5</div>
+				<div id="Volume">Item 6</div>
+				<div id="ScreenMirroring">Item 7</div>
+				<div id="Torch">Item 8</div>
+				<div id="Stopwatch">Item 9</div>
+				<div id="Calculator">Item 10</div>
+				<div id="Camera">Item 11</div>
+				<div id="ScanQR">Item 12</div>
+			</div>
+		);
 	}
 }
 
