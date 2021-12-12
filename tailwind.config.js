3e78559c8483ac3eff67c80b558bd011fed6cd2f
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-12-12 16:24:07 +0530

    Color adjestment and timing changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,25 @@ module.exports = {
 	content: ["./src/**/*.{js,jsx}"],
 	theme: {
 		extend: {
+			colors: {
+				gray: {
+					800: "#717171",
+					700: "#909090",
+				},
+			},
 			animation: {
-				wiggle: "wiggle 500ms ease-in-out",
+				wiggle: "wiggle1 700ms ease-in-out",
 			},
 			keyframes: {
 				wiggle: {
 					"0%, 100%": { transform: "rotate(0deg)" },
 					"25%, 75%": { transform: "rotate(10deg)" },
 					"50%": { transform: "rotate(-10deg)" },
+				},
+				wiggle1: {
+					"0%": { transform: "rotate(0deg)" },
+					"25%": { transform: "rotate(-45deg)" },
+					"100%": { transform: "rotate(360deg)" },
 				},
 			},
 		},
