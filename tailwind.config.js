3bda1051234c110209f38140f8f32224f424a8f9
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-12-12 15:59:27 +0530

    Added the wiggle animation back in

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				wiggle: "wiggle 500ms ease-in-out",
+			},
+			keyframes: {
+				wiggle: {
+					"0%, 100%": { transform: "rotate(0deg)" },
+					"25%, 75%": { transform: "rotate(10deg)" },
+					"50%": { transform: "rotate(-10deg)" },
+				},
+			},
+		},
 	},
 	plugins: [],
 };
